3d402b4fe784cc427920f4abc9595c18d97d2ba6
Skr99 <42551174+Skr99@users.noreply.github.com>
2020-10-01 13:24:18 +0530

    Create HelloWord.cpp

---
+++ HelloWord.cpp
@@ -0,0 +1,14 @@
+/*
+Very basic program in any programming language is Hello World.
+So in this code I have write to print Hello World!. 
+This is C++ Code.
+*/
+
+#include<bits/stdc++.h>   // This is the header file of C++ which stores all the necessary pre-processing file for the execution
+using namespace std;
+
+// main is the function from where any program begin to execute 
+
+int main(){
+  cout << "Hello World !";  // cout is used to print 'Hello Word !' on the console output.
+}
